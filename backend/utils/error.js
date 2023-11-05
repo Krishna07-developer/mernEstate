6eaf4f542ddea32ce12ad39dfcf1adf58549df0b
unpairedkrish-7 <krishnabhukya893@gmail.com>
2023-11-05 17:36:43 +0530

    create sign in api route

--- backend/utils/error.js
+++ backend/utils/error.js
@@ -1,3 +1,7 @@
-export const error = ()=>{
-    
+export const errorHandler = (statusCode,message)=>{
+    const error = new Error();
+    error.statusCode = statusCode;
+    error.message = message;
+    return error;
+
 }
