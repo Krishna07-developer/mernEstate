c28baf7ddf4bef26bab6beecd7b8b059902ada8e
unpairedkrish-7 <krishnabhukya893@gmail.com>
2023-11-07 18:23:11 +0530

    add delete user functionality

--- fronted/src/pages/Profile.jsx
+++ fronted/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import {useSelector } from "react-redux"
 import {useRef , useState ,useEffect} from "react";
 import {getStorage, ref,uploadBytesResumable , getDownloadURL} from "firebase/storage"
 import { app } from '../firebase.js';
-import {updateUserStart,updateUserFailure,updateUserSuccess} from "../redux/user/userSlice.js"
+import {updateUserStart,updateUserFailure,updateUserSuccess, deleteUserFailure, deleteUserStart, deleteUserSuccess} from "../redux/user/userSlice.js"
 import { useDispatch } from "react-redux";
 
 const Profile = () => {
@@ -82,6 +82,27 @@ const handleSubmit= async (e)=>{
     dispatch(error.message)
   }
 }
+
+
+const handleDeleteUser=async ()=>{
+  try {
+    dispatch(deleteUserStart());
+    const res = await fetch(`/api/user/delete/${currentUser._id}` , {
+      method : "DELETE"
+    });
+
+    const data = await res.json();
+    if(data.message === false){
+      dispatch(deleteUserFailure(data.message))
+      return;
+    }
+
+    dispatch(deleteUserSuccess(data))
+    
+  } catch (error) {
+    dispatch(deleteUserFailure(error.message))
+  }
+}
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -105,7 +126,7 @@ const handleSubmit= async (e)=>{
         <button disabled={loading} className="bg-slate-700 text-white rounded-lg p-3 hover:opacity-95 disabled:opacity-90 uppercase">{loading ? "Loading..." : "Update"}</button>
       </form>
       <div className="flex justify-between mt-5"> 
-        <span className="text-red-700 cursor-pointer">Delete account</span>
+        <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete account</span>
         <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
       <p className="text-red-700 mt-5">{error ? error : ""}</p>
