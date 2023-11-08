99efec0022193cd236a293a5bc7978a5c9a66f8f
unpairedkrish-7 <krishnabhukya893@gmail.com>
2023-11-08 11:10:12 +0530

    complete create-listing Ui

--- fronted/src/pages/Profile.jsx
+++ fronted/src/pages/Profile.jsx
@@ -4,6 +4,7 @@ import {getStorage, ref,uploadBytesResumable , getDownloadURL} from "firebase/st
 import { app } from '../firebase.js';
 import {updateUserStart,updateUserFailure,updateUserSuccess, deleteUserFailure, deleteUserStart, deleteUserSuccess, signInStart, signInFailure, signInSuccess, signOutUserStart, signOutUserFailure, signOutUserSuccess} from "../redux/user/userSlice.js"
 import { useDispatch } from "react-redux";
+import {Link} from "react-router-dom";
 
 const Profile = () => {
   const {currentUser , loading,error} = useSelector(state => state.user);
@@ -131,8 +132,6 @@ const handleSignOut=async ()=>{
             filePerc > 0 && filePerc < 100 ? 
               (<span className="text-slate-700">{`Uploading ${filePerc}%`}</span>) : filePerc === 100 ? (
                 <span className="text-green-700">Image Successfully  Uploaded!</span>) : ("")
-              
-          
           }
         </p>
 
@@ -140,6 +139,9 @@ const handleSignOut=async ()=>{
         <input type="email" placeholder="email" id="email" defaultValue={currentUser.email} className="border p-3 rounded-lg"  onChange={handleChange}/>
         <input type="password" placeholder="password" id="password" className="border p-3 rounded-lg"   onChange={handleChange} />
         <button disabled={loading} className="bg-slate-700 text-white rounded-lg p-3 hover:opacity-95 disabled:opacity-90 uppercase">{loading ? "Loading..." : "Update"}</button>
+        <Link className="bg-green-700 text-white p-3 text-center rounded-lg uppercase hover:opacity-95" to={`/create-listing`}>
+          create listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5"> 
         <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete account</span>
