659e4a624eb01123e60efe0a43d46d4b88d264ea
unpairedkrish-7 <krishnabhukya893@gmail.com>
2023-11-14 11:00:31 +0530

    complete about page

--- fronted/src/pages/About.jsx
+++ fronted/src/pages/About.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800 '>About Krishna Estate</h1>
+      <p className='mb-4 text-slate-700'>Krishna Estate is a leading real estate agency that specializes in helping clients buy,sell, and rent properties in the most desirable neighbourhoods.
+      Our team of experienced agents is dedicated to providing exceptional services and making the buying and selling process as smooth as possible.</p>
+      <p className='mb-4 text-slate-700'>Our mission is to help our clients achieve their real estate goals by providing expert advice , personalized service, and a deep understanding of the local market. Whether you are looking to buy, sell or rent a property, We are here to help you every step of the way. </p>
+    </div>
   )
 }
 
